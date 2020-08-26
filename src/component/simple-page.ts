a1dcf9f91d044e2a9dc8e92c8a780259b27e7d21
Joseph Spurrier <github@josephspurrier.com>
2020-08-26 19:15:49 -0400

    Add margin to top of error page

--- src/component/simple-page.ts
+++ src/component/simple-page.ts
@@ -9,7 +9,7 @@ export const SimplePage: m.Component<Attrs> = {
   view: ({ attrs, children }) =>
     m('div', [
       m('section', [
-        m('div', { class: 'container is-fluid' }, [
+        m('div', { class: 'container is-fluid mt-4' }, [
           m('h1', { class: 'title' }, attrs.title),
           m('h2', { class: 'subtitle' }, attrs.description),
           children,
